9d513bc84b4ebf1d4e397311567bb7bfce7b4c15
Nasiq Ziyan <nasiq112@gmail.com>
2022-07-29 21:19:19 +0100

    Add pen color functionality with Pickr library

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ let gridSize = document.querySelector('.gridSize');
 let clearGrid = document.querySelector("#clearGrid");
 let gridLines = document.querySelector('.gridLines');
 let board = document.querySelector(".board");
+let pcrButton = document.querySelector('pcr-button');
 
 selectValue.textContent = slider.value
 
@@ -55,13 +56,101 @@ gridLines.addEventListener('click', () => {
 
 clearGrid.onclick = () => changeSize(slider.value);
 
+// Use Pickr Library for creating colour picker
+
+const pickr = Pickr.create({
+  el: '.color-picker',
+  theme: 'classic', // or 'monolith', or 'nano'
+  default: '#000000',
+
+  swatches: [
+      'rgba(244, 67, 54, 1)',
+      'rgba(233, 30, 99, 0.95)',
+      'rgba(156, 39, 176, 0.9)',
+      'rgba(103, 58, 183, 0.85)',
+      'rgba(63, 81, 181, 0.8)',
+      'rgba(33, 150, 243, 0.75)',
+      'rgba(3, 169, 244, 0.7)',
+      'rgba(0, 188, 212, 0.7)',
+      'rgba(0, 150, 136, 0.75)',
+      'rgba(76, 175, 80, 0.8)',
+      'rgba(139, 195, 74, 0.85)',
+      'rgba(205, 220, 57, 0.9)',
+      'rgba(255, 235, 59, 0.95)',
+      'rgba(255, 193, 7, 1)'
+  ],
+
+  components: {
+
+      // Main components
+      preview: true,
+      opacity: true,
+      hue: true,
+
+      // Input / output Options
+      interaction: {
+          hex: true,
+          rgba: true,
+          input: true,
+      }
+  }
+});
+
+const pickr2 = Pickr.create({
+  el: '.color-picker2',
+  theme: 'classic', // or 'monolith', or 'nano'
+  default: '#ffffff',
+  
+
+  swatches: [
+      'rgba(244, 67, 54, 1)',
+      'rgba(233, 30, 99, 0.95)',
+      'rgba(156, 39, 176, 0.9)',
+      'rgba(103, 58, 183, 0.85)',
+      'rgba(63, 81, 181, 0.8)',
+      'rgba(33, 150, 243, 0.75)',
+      'rgba(3, 169, 244, 0.7)',
+      'rgba(0, 188, 212, 0.7)',
+      'rgba(0, 150, 136, 0.75)',
+      'rgba(76, 175, 80, 0.8)',
+      'rgba(139, 195, 74, 0.85)',
+      'rgba(205, 220, 57, 0.9)',
+      'rgba(255, 235, 59, 0.95)',
+      'rgba(255, 193, 7, 1)'
+  ],
+
+  components: {
+
+      // Main components
+      preview: true,
+      opacity: true,
+      hue: true,
+
+      // Input / output Options
+      interaction: {
+          hex: true,
+          rgba: true,
+          input: true,
+      }
+  }
+});
+
+// Make Chosen Pen Color Functional
+
+let userColour = '#000000'
 
 function changeColour(e) {
   console.log("type: " + e.type + " " + "mouseDown: " + mouseDown)
   if (e.type === 'mouseover' && !mouseDown) return
   
-  else (e.target.style.backgroundColor = 'red')
+  else {
+    pickr.on('change', (color) => {userColour = color.toRGBA().toString();})  
+    e.target.style.backgroundColor = userColour;
+    pickr.applyColor(true) // Does the same functionality as save (changes the color of the pickr box)
+  }
 }
+
+
 
 function changeSize(size) {
   populateBoard(size)
